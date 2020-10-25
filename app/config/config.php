628022400c5b94fc7327e535be55092a6044d6a5
Rosie Faulkner <rosiefaulkner@0069L2Z7CRMLCVF.attlocal.net>
2020-10-25 18:40:29 -0400

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'rosie');
+  define('DB_PASS', 'root');
+  define('DB_NAME', 'tmvc');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/faulknermvc');
+  // Site Name
+  define('SITENAME', 'FaulknerMVC');
