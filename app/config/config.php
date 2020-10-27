a5a04509a3074a8a57eeca4a880f3e84186c8137
Rosie Faulkner <rosiefaulkner@0069L2Z7CRMLCVF.local>
2020-10-26 23:01:21 -0400

    form validation for login and register

--- app/config/config.php
+++ app/config/config.php
@@ -8,7 +8,7 @@
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
-  define('URLROOT', 'http://localhost:8881');
+  define('URLROOT', 'http://localhost/shareposts');
   // Site Name
   define('SITENAME', 'SharePosts');
   // App Version
