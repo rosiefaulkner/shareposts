e6a2ac1b712661fe0aca29c2239a3d4ac60dd27b
Rosie Faulkner <rosiefaulkner@0069L2Z7CRMLCVF.local>
2020-11-02 23:14:07 -0500

    creates logout

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -164,4 +164,20 @@
       $_SESSION['user_name'] = $user->name;
       redirect('pages/index');
     }
+
+    public function logout(){
+      unset($_SESSION['user_id']);
+      unset($_SESSION['user_email']);
+      unset($_SESSION['user_name']);
+      session_destroy();
+      redirect('users/login');
+    }
+
+    public function isLoggedIn(){
+      if(isset($_SESSION['user_id'])){
+        return true;
+      } else {
+        return false;
+      }
+    }
   }
