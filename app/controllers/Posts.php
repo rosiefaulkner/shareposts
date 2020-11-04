556696b291ebff30ade70f9f92a5eef5fefff0ec
Rosie Faulkner <rosiefaulkner@0069L2Z7CRMLCVF.local>
2020-11-03 23:18:29 -0500

    adds editing functionality to post

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -65,6 +65,62 @@
       }
     }
 
+    public function edit($id){
+      if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        // Sanitize POST array
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+        $data = [
+          'title' => trim($_POST['title']),
+          'body' => trim($_POST['body']),
+          'user_id' => $_SESSION['user_id'],
+          'title_err' => '',
+          'body_err' => ''
+        ];
+
+        // Validate data
+        if(empty($data['title'])){
+          $data['title_err'] = 'Please enter title';
+        }
+        if(empty($data['body'])){
+          $data['body_err'] = 'Please enter body text';
+        }
+
+        // Make sure no errors
+        if(empty($data['title_err']) && empty($data['body_err'])){
+          // Validated
+          if($this->postModel->addPost($data)){
+            flash('post_message', 'Post Added');
+            redirect('posts');
+          } else {
+            die('Something went wrong');
+          }
+        } else {
+          // Load view with errors
+          $this->view('posts/add', $data);
+        }
+
+      } else {
+        // Get existing post from model
+        $post = $this->postModel->getPostById($id);
+
+        // Check for owner
+        if($post->user_id != $_SESSION['user_id']){
+          flash('You may only edit your own posts');
+          redirect('posts');
+        }
+
+        $data = [
+          'id' => $id,
+          'title' => $post->title,
+          'body' => $post->body
+        ];
+  
+        $this->view('posts/edit', $data);
+      }
+    }
+////////////////////////
+
     public function show($id){
       $post = $this->postModel->getPostById($id);
       $user = $this->userModel->getUserById($post->user_id);
